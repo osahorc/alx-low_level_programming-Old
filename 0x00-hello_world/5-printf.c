9ea570ce0608052a95edb552bcfa4dc9e00015b6
Chukwuma <osahorc@gmail.com>
2022-09-18 09:58:19 -0700

    M

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main:- This is the main function
+ * Description: This program shows positive of negative numbers
+ * Return:0 - Returns Zero
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
