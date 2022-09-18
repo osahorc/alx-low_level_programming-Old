0781ba43223840df168f63f7a6b71ec6386e3d94
Chukwuma <osahorc@gmail.com>
2022-09-18 10:46:26 -0700

    M

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main:- This is the main function
+ * Description: This program shows positive of negative numbers
+ * Return:0 - Returns Zero
+ */
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
