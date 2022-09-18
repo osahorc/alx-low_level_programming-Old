0ce5a9bdf1c8d14d88b2be2c8f70e4289addc81b
Chukwuma <osahorc@gmail.com>
2022-09-18 10:04:10 -0700

    M

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main:- This is the main function
+ * Description: This program shows positive of negative numbers
+ * Return:1 - Returns One
+ */
+int main(void)
+{
+	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
