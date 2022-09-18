3bf549875a9fabbc33ff3bb9ad2444cb8304a936
Chukwuma <osahorc@gmail.com>
2022-09-18 06:13:20 -0700

    my commit

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -10,7 +10,7 @@ int main(void)
 	char lower;
 
 	for (lower = 'a'; lower <= 'z'; lower++)
-		if (lower ! = 'q' && lower ! = 'e')
+		if (lower !='q' && lower !='e')
 			putchar(lower);
 	putchar('\n');
 	return (0);
