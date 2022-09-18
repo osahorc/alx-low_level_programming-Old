05496d626f2cbf86c5b19dd1e24869630957a4a1
Chukwuma <osahorc@gmail.com>
2022-09-18 06:33:06 -0700

    my commit

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main:- This is the main function
+ * Description: This program shows positive of negative numbers
+ * Return:0 - Returns Zero
+ */
+int main(void)
+{
+	int lower;
+
+	for (lower =0'; lower <= 9; lower++)
+		printf("%d", lower);
+	putchar('\n');
+	return (0);
+}
