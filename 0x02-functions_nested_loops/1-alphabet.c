4f4c0d909f2a155869aa285e4a2328fcd939a650
Chukwuma <osahorc@gmail.com>
2022-09-18 10:58:43 -0700

    M

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <main.h>
+
+/**
+ * main:- This is the main function
+ * Description: This program shows positive of negative numbers
+ * Return:0 - Returns Zero
+ */
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
