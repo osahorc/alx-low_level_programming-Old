11981c4e0813bd50b6f1d977ab50086182b3bfa1
Chukwuma <osahorc@gmail.com>
2022-09-18 05:35:45 -0700

    my commit

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main:- This is the main function
+ * Description: This program shows positive of negative numbers
+ * Return:0 - Returns Zero
+ */
+int main(void)
+{
+	char lower;
+	for (lower ='a'; lower <='z'; lower++)
+		putchar(lower);
+	putchar('\n');
+	return 0;
+}
