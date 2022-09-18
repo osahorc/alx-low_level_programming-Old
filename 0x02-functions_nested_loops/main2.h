98464085b9704ddbca81f8e57b833bff013c8a02
Chukwuma <osahorc@gmail.com>
2022-09-18 12:31:26 -0700

    M

---
+++ 0x02-functions_nested_loops/main2.h
@@ -0,0 +1,12 @@
+#include <unistd.h>
+
+/**
+ * print_alphabet - writes the character c to stdout
+ * description:- The character to print
+ * Return: On successnull
+ * On error, null is returned, and errno is set appropriately.
+ */
+void print_alphabet(void)
+{
+	return;
+}
