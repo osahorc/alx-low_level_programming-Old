1d639e5f55c6c0f0b7b13498895aaa36a4d97b90
Chukwuma <osahorc@gmail.com>
2022-09-19 12:15:25 -0700

    my commit

---
+++ 0x05-pointers_arrays_strings/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_alphabet - This is the main function
+ *
+ * Return: - Returns void
+ */
+void print_alphabet(void)
+{
+	int c;
+
+	for (c = 'a'; c <= 'z'; c++)
+		_putchar(c);
+	_putchar('\n');
+}
