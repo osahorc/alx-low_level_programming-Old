90f81c8cee024498e36d3d153855d20829bdf882
Chukwuma <osahorc@gmail.com>
2022-09-20 11:37:24 -0700

    m

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _strlen - This function counts the lenght of the string
+ * @s: the string to be calculated
+ *
+ * Return: void
+ */
+
+int _strlen(char *s);
+{
+	strlen(*s);
+}
