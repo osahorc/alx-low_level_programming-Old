87429625d969efa3bd0af6edf7cee1a24e7366ea
Chukwuma <osahorc@gmail.com>
2022-09-21 13:16:03 -0700

    m

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,5 +10,6 @@ void print_alphabet_x10(void)
 	int ti;
 	ti = 0;
 	for (ti = 0; ti <= 9; ti++)
-	{{char ch; for (ch = 'a'; ch <= 'z'; ch++) _putchar(ch);_putchar('\n');}}
-
+	{{char ch; for (ch = 'a'; ch <= 'z'; ch++)
+		 _putchar(ch);_putchar('\n');}}
+}
