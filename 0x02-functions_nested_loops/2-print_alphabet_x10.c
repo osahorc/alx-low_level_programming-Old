5c1cb05d1875715e4d6d344e8d1827e9e1eb7eeb
Chukwuma <osahorc@gmail.com>
2022-09-21 04:28:31 -0700

    m

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_alphabet - This is the main function
+ *
+ * Return: - Returns void
+ */
+void print_alphabet(void)
+{
+	int d;
+	d = 0;
+	while (d <= 9) 
+	{
+	int c;
+	c = 'a'
+	while (c <= 'z')
+		{
+			_putchar(c);
+			c++;
+		}
+	}
+	_putchar('\n');
+}
