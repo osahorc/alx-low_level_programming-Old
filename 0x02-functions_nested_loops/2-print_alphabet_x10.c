788505bd70a05a9a0308e9eef4517ec491c93681
Chukwuma <osahorc@gmail.com>
2022-09-22 00:33:00 -0700

    m

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -16,7 +16,11 @@ void print_alphabet_x10(void)
 
 			{
 				{
-					char ch; for (ch = 'a'; ch <= 'z'; ch++) _putchar(ch);				 _putchar('\n');
-				}			}
+					char ch; 
+						for (ch = 'a'; ch <= 'z'; ch++) 
+							_putchar(ch);
+	       						_putchar('\n');
+				}			
+			}
 
 }
